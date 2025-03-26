e0e1116bbfc0df81dcf57a7b98119ceca64b79bb
Whatdoyumin <lym4963@naver.com>
2025-03-26 15:16:16 +0900

    feat: 기본 과제 구현

---
+++ task/20250326/기본/style-test/src/components/CheckBox1.vue
@@ -0,0 +1,24 @@
+<template lang="">
+  <div>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="$emit('check-chaged', { id, checked: $event.target.checked })"
+    />
+    <span
+      v-if="checked === true"
+      style="color: blue; text-decoration: underline"
+    >
+      <i>{{ label }}</i>
+    </span>
+    <span v-else style="color: gray">{{ label }}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Checkbox1',
+  props: ['id', 'checked', 'label'],
+};
+</script>
+<style lang=""></style>
