8480fe8e3d23806a73efc103e4ed0823fedba4e3
Whatdoyumin <lym4963@naver.com>
2025-03-26 09:33:45 +0900

    chore: 컴포넌트 스타일 개발 환경 구현

---
+++ lecture/chapter08/style-test/src/App.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div>
+    <Child1 />
+    <Child2 />
+    <Child3 />
+  </div>
+</template>
+<script>
+import Child1 from './components/Child1.vue';
+import Child2 from './components/Child2.vue';
+import Child3 from './components/Child3.vue';
+
+export default {
+  name: 'app',
+  components: { Child1, Child2, Child3 },
+};
+</script>
+<style lang=""></style>
