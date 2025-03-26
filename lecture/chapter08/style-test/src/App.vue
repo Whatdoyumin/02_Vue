38e762a726d6b2f17d6e9be5f2bd37d96863934e
Whatdoyumin <lym4963@naver.com>
2025-03-26 10:19:52 +0900

    feat: 컴포넌트 slot 실습 구현

--- lecture/chapter08/style-test/src/App.vue
+++ lecture/chapter08/style-test/src/App.vue
@@ -3,16 +3,20 @@
     <Child1 />
     <Child2 />
     <Child3 />
+    <NoSlotTest />
+    <SlotTest />
   </div>
 </template>
 <script>
 import Child1 from './components/Child1.vue';
 import Child2 from './components/Child2.vue';
 import Child3 from './components/Child3.vue';
+import NoSlotTest from './components/NoSlotTest.vue';
+import SlotTest from './components/SlotTest.vue';
 
 export default {
   name: 'app',
-  components: { Child1, Child2, Child3 },
+  components: { Child1, Child2, Child3, NoSlotTest, SlotTest },
 };
 </script>
 <style lang=""></style>
---
+++ lecture/chapter08/style-test/src/components/CheckBox2.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <div>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="$emit('check-chaged', { id, checked: $event.target.checked })"
+    />
+    <slot>Item</slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Checkbox1',
+  props: ['id', 'checked'],
+};
+</script>
+<style lang=""></style>
