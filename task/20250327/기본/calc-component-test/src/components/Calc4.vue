c5033f67eb529bc42a076cebb6f81e93cd8fd3e8
Whatdoyumin <lym4963@naver.com>
2025-03-27 15:13:38 +0900

    feat: 계산기 computed()로 구현

---
+++ task/20250327/기본/calc-component-test/src/components/Calc4.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div>
+    X: <input type="text" v-model.number="state.x" /><br />
+    Y: <input type="text" v-model.number="state.y" /><br />
+    <br />
+    <button @click="calcAdd">계산</button><br />
+    <div>결과 : {{ result }}</div>
+  </div>
+</template>
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+  name: 'Calc2',
+  setup() {
+    const state = reactive({ x: 10, y: 20 });
+
+    const result = computed(() => {
+      return state.x + state.y;
+    });
+
+    return { state, result };
+  },
+};
+</script>
+<style lang=""></style>
