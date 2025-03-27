eb895179cd491e0eb78ffc0e0a716ade82b6f200
Whatdoyumin <lym4963@naver.com>
2025-03-27 17:41:13 +0900

    fix: 오류 해결

--- task/20250327/심화/todolist-app/src/App.vue
+++ task/20250327/심화/todolist-app/src/App.vue
@@ -1,15 +1,21 @@
-<template lang="">
+<!-- vueinit : vue3 기본 형태 스니펫 -->
+
+<template>
   <div id="app" class="container">
     <div class="card card-body bg-light">
-      <div class="title">🕒 TodoList App (Composition API Version) 💻</div>
+      <div class="title">🕒 TodoList App (Composition API Version)💻</div>
     </div>
+
     <div class="card card-default card-borderless">
       <div class="card-body">
-        <!-- 입력용 컴포넌트 -->
+        <!-- 입력용 컴포넌트 
+          방출(add-todo)된 이벤트가 감지 되었을 때 
+          addTodo() 메서드 호출
+        -->
         <InputTodo @add-todo="addTodo" />
 
         <!-- 카운트 컴포넌트 -->
-        <TodoCount :todolist="todolist" />
+        <TodoCount :todoList="state.todoList" />
 
         <!-- 목록 컴포넌트 -->
         <TodoList
@@ -23,52 +29,68 @@
 </template>
 
 <script setup>
-import InputTodo from './components/InputTodo.vue';
-import TodoCount from './components/TodoCount.vue';
-import TodoList from './components/TodoList.vue';
 import { reactive, onMounted, computed } from 'vue';
 
+// import 태그명 from '경로';
+import InputTodo from './components/InputTodo.vue';
+import TodoList from './components/TodoList.vue';
+import TodoCount from './components/TodoCount.vue';
+
+/* 반응형 데이터 생성 */
 const state = reactive({ todoList: [] });
 
+/* 컴포넌트 마운트 시 실행 */
 onMounted(() => {
-  state.todoList.push({ id: Date.now(), todo: '자전거 타기', completed: false });
-  state.todoList.push({ id: Date.now() + 1, todo: '딸과 공원 산책', completed: true });
+  state.todoList.push({
+    id: Date.now(),
+    todo: '자전거 타기',
+    completed: false,
+  });
+  state.todoList.push({
+    id: Date.now() + 1,
+    todo: '딸과 공원 산책',
+    completed: true,
+  });
   state.todoList.push({
     id: Date.now() + 2,
     todo: '일요일 애견 카페',
     completed: false,
   });
-  state.todoList.push({ id: Date.now() + 3, todo: 'Vue 원고 집필', completed: false });
+  state.todoList.push({
+    id: Date.now() + 3,
+    todo: 'Vue 원고 집필',
+    completed: false,
+  });
 });
 
 /* 할 일 추가 메서드 */
+// receiveTodo : InputTodo 컴포넌트의
+// this.$emit('add-todo', this.todo); 에서 전달된 this.todo(payload)
 const addTodo = (receiveTodo) => {
   state.todoList.push({
     id: Date.now(),
     todo: receiveTodo,
     completed: false,
   });
-  receiveTodo = '';
 };
 
-// 할 일 완료 여부 변경 메서드
+/* 할 일 완료 여부 변경 메서드 */
+// 매개변수 id : TodoListItem 에서 부터 전달된 id
 const toggleCompleted = (id) => {
   let index = state.todoList.findIndex((item) => id === item.id);
   state.todoList[index].completed = !state.todoList[index].completed;
 };
 
 /* 할 일 삭제 메서드 */
+// 매개변수 id : TodoListItem 에서 부터 전달된 id
 const deleteTodo = (id) => {
-  // 배열.findIndex(콜백함수) : 콜백 함수의 결과가 true인 index를 반환
-
   let index = state.todoList.findIndex((item) => id === item.id);
   state.todoList.splice(index, 1);
 };
 
-// 정렬
+// computed() 사용
 const sortedList = computed(() => {
   state.todoList.sort((t1, t2) => {
-    // true > false  -> 결과 : true
     if (t1.completed > t2.completed) {
       return 1; // 양수 반환 시 자리 바뀜 (t1, t2 -> t2, t1)
     }
@@ -76,8 +98,6 @@ const sortedList = computed(() => {
     if (t1.completed < t2.completed) {
       return -1; // 음수 반환 시 자리 바꿈 X
     }
-
-    // 모두 소문자로 변경(대소문자 구분 X)
     const todo1 = t1.todo.toLowerCase();
     const todo2 = t2.todo.toLowerCase();
 
@@ -90,4 +110,3 @@ const sortedList = computed(() => {
   return state.todoList;
 });
 </script>
-<style lang=""></style>
--- task/20250327/심화/todolist-app/src/components/TodoCount.vue
+++ task/20250327/심화/todolist-app/src/components/TodoCount.vue
@@ -1,10 +1,9 @@
-<template lang="">
+<template>
   <div class="row mb-3">
     <div class="col-md text-center">
       전체 todo 개수 : <span>{{ todoList.length }}</span>
     </div>
     <div class="col-md text-center">
-      <!-- 완료 todo 개수 : <span :class="completedCount && completed">{{completedCount}}</span> -->
       완료 todo 개수 :
       <span :class="{ completed: completedCount > 0 }">{{ completedCount }}</span>
     </div>
@@ -16,16 +15,21 @@
     </div>
   </div>
 </template>
+
 <script setup>
+import { computed } from 'vue';
+
+// 부모로 부터 데이터 전달 받기 + 유효성 검사
 const props = defineProps({
-  // 전달 받은 todoList는 배열 형태, 필수 데이터
-  todoList: { type: Array, required: true },
+  todoList: {
+    type: Array,
+    required: true,
+  },
 });
 
 // 완료 개수 카운트
-const completedCount = () => {
-  // todoList의 요소 중 completed 가 true인 요소만 남긴 배열 반환
-  return todoList.filter((todo) => todo.completed).length;
-};
+const completedCount = computed(() => {
+  // todolist의 요소 중 completed 가 true인 요소만 남긴 배열 반환
+  return props.todoList.filter((todo) => todo.completed).length;
+});
 </script>
-<style lang=""></style>
