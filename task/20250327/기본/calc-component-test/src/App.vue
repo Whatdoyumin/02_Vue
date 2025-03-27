c4d4e83734c8f33b4c8159ef118f469c3af71e7d
Whatdoyumin <lym4963@naver.com>
2025-03-27 15:10:09 +0900

    feat: 계산기 reactive()로 구현

--- task/20250327/기본/calc-component-test/src/App.vue
+++ task/20250327/기본/calc-component-test/src/App.vue
@@ -3,15 +3,18 @@
     <Calc />
     <br />
     <Calc2 />
+    <br />
+    <Calc3 />
   </div>
 </template>
 <script>
 import Calc from './components/Calc.vue';
 import Calc2 from './components/Calc2.vue';
+import Calc3 from './components/Cal3.vue';
 
 export default {
   name: 'App',
-  components: { Calc, Calc2 },
+  components: { Calc, Calc2, Calc3 },
 };
 </script>
 <style lang=""></style>
