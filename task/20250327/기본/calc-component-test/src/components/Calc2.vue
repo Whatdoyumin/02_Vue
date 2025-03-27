f7937333766569551ed187712df9992d6e93cd80
Whatdoyumin <lym4963@naver.com>
2025-03-27 15:08:38 +0900

    feat: 계산기 setup()으로 구현

--- task/20250327/기본/calc-component-test/src/components/Calc2.vue
+++ task/20250327/기본/calc-component-test/src/components/Calc2.vue
@@ -2,17 +2,26 @@
   <div>
     X: <input type="text" v-model.number="x" /><br />
     Y: <input type="text" v-model.number="y" /><br />
+    <br />
+    <button @click="calcAdd">계산</button><br />
+    <div>결과 : {{ result }}</div>
   </div>
 </template>
 <script>
 import { ref } from 'vue';
 
 export default {
-  name: 'Calc',
+  name: 'Calc2',
   setup() {
     const x = ref(10);
     const y = ref(20);
-    return { x, y };
+    const result = ref(30);
+
+    const calcAdd = () => {
+      result.value = x.value + y.value;
+    };
+
+    return { x, y, result, calcAdd };
   },
 };
 </script>
