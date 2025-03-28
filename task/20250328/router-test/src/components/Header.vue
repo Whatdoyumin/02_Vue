890b9eb078da630f8ab4ebd5026f2953edb08187
Whatdoyumin <lym4963@naver.com>
2025-03-28 15:29:52 +0900

    feat: 헤더 구현

--- task/20250328/router-test/src/components/Header.vue
+++ task/20250328/router-test/src/components/Header.vue
@@ -1,9 +1,46 @@
 <template lang="">
-  <div></div>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark mt-2">
+    <span class="navbar-brand">이날치(LeeNalChi)</span>
+    <button class="navbar-toggle" type="button" @click="changeIsNavShow">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
+
+  <div :class="navClass">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/">홈</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">소개</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/members">멤버</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/videos">영상</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   name: 'Header',
+
+  setup() {
+    const state = reactive({
+      isNavShow: false,
+    });
+
+    const navClass = computed(() =>
+      state.isNavShow ? 'callapse navbar-collapse show' : 'collapse navbar-collapse',
+    );
+    const changeIsNavShow = () => {
+      state.isNavShow = !state.isNavShow;
+    };
+
+    return { state, changeIsNavShow, navClass };
+  },
 };
 </script>
 <style lang=""></style>
