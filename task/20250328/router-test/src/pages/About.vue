5431a0e48dc1717f024b5d4913fb1152805f61aa
Whatdoyumin <lym4963@naver.com>
2025-03-28 16:48:00 +0900

    feat: about 경로 setup() 함수로 변경

--- task/20250328/router-test/src/pages/About.vue
+++ task/20250328/router-test/src/pages/About.vue
@@ -1,14 +1,24 @@
 <template lang="">
   <div class="card card-body">
     <h2>About</h2>
-    <p>요청 경로: {{ $route.fullPath }}</p>
+    <!-- <p>요청 경로: {{ $route.fullPath }}</p> -->
+    <p>요청 경로: {{ route.fullPath }}</p>
   </div>
 </template>
 <script>
+import { useRoute } from 'vue-router';
+
 export default {
   name: 'About',
-  created() {
-    console.log(this.$route);
+  // created() {
+  //   console.log(this.$route);
+  // },
+
+  setup() {
+    const route = useRoute();
+    console.log(route.fullPath);
+
+    return { route };
   },
 };
 </script>
