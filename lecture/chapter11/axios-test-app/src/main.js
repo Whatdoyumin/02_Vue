f8e869593a9aea8836c4e4867c1d0e14900ae91f
Whatdoyumin <lym4963@naver.com>
2025-04-02 11:41:39 +0900

    feat: axios 에러 처리 실습

--- lecture/chapter11/axios-test-app/src/main.js
+++ lecture/chapter11/axios-test-app/src/main.js
@@ -5,11 +5,25 @@ import { createApp } from 'vue';
 
 // createApp(App).mount('#app')
 
-// axios 비동기 처리 실습
-// import App2 from './App2.vue';
+/* axios 비동기 처리 실습
+import App2 from './App2.vue';
 
-// createApp(App2).mount('#app');
+createApp(App2).mount('#app');
+*/
 
+/* axios post 실습
 import App4 from './App4.vue';
 
 createApp(App4).mount('#app');
+*/
+
+/* axios 에러 처리 실습
+import App5 from './App5.vue';
+
+createApp(App5).mount('#app');
+*/
+
+/* axios 에러 처리 실습2 */
+import App6 from './App6.vue';
+
+createApp(App6).mount('#app');
