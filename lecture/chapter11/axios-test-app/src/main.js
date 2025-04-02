074bcc7130cac6d73b8ff4db46fb833f876bcd61
Whatdoyumin <lym4963@naver.com>
2025-04-02 11:15:44 +0900

    feat: axios 비동기 처리 실습

--- lecture/chapter11/axios-test-app/src/main.js
+++ lecture/chapter11/axios-test-app/src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+import App2 from './App2.vue';
+
+createApp(App2).mount('#app');
