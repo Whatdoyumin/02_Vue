64516b255d708707d5782e11a6c7826ba662cdd5
Whatdoyumin <lym4963@naver.com>
2025-04-03 13:28:15 +0900

    feat: todoList 목록 전체 조회 구현

--- lecture/chapter11/todo-axios-test/src/pages/All.vue
+++ lecture/chapter11/todo-axios-test/src/pages/All.vue
@@ -1,5 +1,51 @@
 <template>
-  <h1>All</h1>
+  <h1>All (전체 조회)</h1>
+  <table class="table table-borderedß">
+    <thead>
+      <th>Done</th>
+      <th>ID</th>
+      <th>Todo</th>
+      <th>Description</th>
+      <th>Delete</th>
+    </thead>
+
+    <tbody>
+      <tr v-for="todo in state.todos" :key="todo.id">
+        <td>
+          <input
+            type="checkbox"
+            :checked="todo.done"
+            @change="updateTodo(todo)"
+          />
+        </td>
+
+        <td>{{ todo.id }}</td>
+        <td>{{ todo.todo }}</td>
+        <td>{{ todo.desc }}</td>
+        <td>
+          <button @click="deleteTodo(todo)">삭제</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
-<script setup></script>
+<script setup>
+import { reactive } from 'vue';
+import axios from 'axios';
+
+// 상태 변수 선언 -> 값 변경 시 반응 -> 컴포넌트 다시 렌더링
+const state = reactive({
+  todos: [],
+});
+
+// 비동기로 서버로부터 모든 할 일 목록을 얻어오기
+const fetchTodoList = async () => {
+  // 'api' === 'http:/'
+  const response = await axios.get('/api/todos');
+
+  state.todos = response.data;
+};
+
+fetchTodoList();
+</script>
